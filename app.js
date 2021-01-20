b592cabf3d738e8ec2c3aa36186e9f41dc92dc0b
Russell Telen <rtelen@my.bcit.ca>
2021-01-20 11:56:21 -0800

    Initial config for app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+// =============================================
+// REQUIRE
+// =============================================
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+
+// ==============================================
+// CONFIG
+// =============================================
+// init express
+const app = express();
+
+// Serving static assets
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Parsing Middlewares
+app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded
+app.use(express.json()); // JSON
+
+// EJS Mate -> for layouts
+app.engine("ejs", ejsMate);
+
+// Setting up the views directory
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// Override with POST having ?_method=PATCH or DELETE
+app.use(methodOverride("_method"));
+
+// Connect Mongoose
+let localDb = "mongodb://localhost:27017/courts";
+let atlasDb = process.env.db;
+mongoose
+  .connect(localDb, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTED!");
+  })
+  .catch((e) => {
+    console.log("MONGO CONNECTION ERROR :(");
+    console.log(e);
+  });
+
+// ==============================================
+// ROUTES
+// =============================================
+// get
+// home page
+app.get("/", (req, res) => {
+  res.send("It works!");
+});
+
+// ==============================================
+// LISTEN
+// =============================================
+let port = process.env.port;
+
+if (port == null || port == "") {
+  port = 5000;
+}
+
+app.listen(port, () => {
+  console.log(`Connected to Port ${port}! http://localhost:${port}/`);
+});
