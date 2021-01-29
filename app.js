fac300f383fc0227db7de1716e60a77564dd591f
Russell Telen <rtelen@my.bcit.ca>
2021-01-28 21:43:09 -0800

    Migrate to cloud db

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ dotenv.config();
 let localDb = "mongodb://localhost:27017/courts";
 let atlasDb = process.env.db;
 mongoose
-  .connect(localDb, {
+  .connect(atlasDb, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
