f4b2cf9b5fd2f4fa0563020475b16f7ae24f1a42
Russell Telen <rtelen@my.bcit.ca>
2021-02-07 11:52:55 -0800

    Define User model

---
+++ models/users.js
@@ -0,0 +1,28 @@
+//==========================================
+// REQUIRE
+//==========================================
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+//==========================================
+// SET UP SCHEMA
+//==========================================
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+// this plugin adds a username, hash and salt field to store the username, the hashed password and the salt value.
+userSchema.plugin(passportLocalMongoose);
+
+//==========================================
+// SET UP MODEL
+//==========================================
+const User = mongoose.model("User", userSchema);
+
+// EXPORT
+module.exports = User;
